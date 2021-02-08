b3f1758a7c473d7536a9c596cff0b99db0502fe5
LoickHommet <59056586+LoickHommet@users.noreply.github.com>
2021-02-08 14:51:17 +0100

    ci(approche-objet):créer une classe

---
+++ src/essais/TestAdressePostale.java
@@ -0,0 +1,9 @@
+package essais;
+
+public class TestAdressePostale {
+    public static void main(String[] args) {
+        TestAdressePostale adresse1 = new TestAdressePostale(65 , "belle rue", 92658);
+        TestAdressePostale adresse2 = new TestAdressePostale(64 , "moche rue", 95684);
+
+    }
+}
