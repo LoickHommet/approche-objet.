6c1b2fd653eb330e4c45510b49148e9c44ddd2c6
LoickHommet <59056586+LoickHommet@users.noreply.github.com>
2021-02-08 14:43:27 +0100

    frist commit

---
+++ src/entites/AdressePostale.java
@@ -0,0 +1,14 @@
+package entites;
+
+public class AdressePostale {
+
+    private int numéroRue;
+    private String libelléRue;
+    private int codePostal;
+
+    public AdressePostale(int numéroRue, String libelléRue, int codePostal) {
+        this.numéroRue = numéroRue;
+        this.libelléRue = libelléRue;
+        this.codePostal = codePostal;
+    }
+}
